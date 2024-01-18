e837cd11ee40127f00403be7520ab2d23daeb6f3
Garcia Dominguez <a.b.garcia.dominguez@accenture.com>
2024-01-18 09:53:28 +0100

    new-version

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ weight = st.number_input("Weight")
 eye_color = st.selectbox("Select eye color", ("Brown", "Blue"))
 
 if st.button('Submit'):
-	x = pd.DataFrame([[height, weight, eye_color]], columns=["Height", "Weight", "Eye"])
-	x = x.replace(["Brown", "Blue"], [1, 0])
-	prediction = clf.predict(x)[0]
-	st.text(f"This pet is a {prediction}")
+    x = pd.DataFrame([[height, weight, eye_color]], columns=["Height", "Weight", "Eye"])
+    x = x.replace(["Brown", "Blue"], [1, 0])
+    prediction = clf.predict(x)[0]
+    st.text(f"This pet is a {prediction}")
